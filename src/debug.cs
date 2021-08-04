d03233911628ff51b64d025fdcfb043492283f58
approachcircle <osc-mcd@protonmail.com>
2021-08-04 13:36:22 +0100

    add debugging functionality

---
+++ src/debug.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Text;
+
+namespace bluebird {
+    namespace debug {
+        class debugClass {
+            public void evaluate(string data = "", string action = "write") {
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine("--debugging session started--");
+                if (action == "write") {
+                    Console.WriteLine("data is: \"{0}\"", data);
+                }
+                Console.WriteLine("--debugging session ended--");
+                Console.ResetColor();
+            }
+            // probably will add more functions in the future
+        }
+    }
+}
