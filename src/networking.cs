ab0295ef0332b3975086b32bf677a182d3ddc7b2
approachcircle <osc-mcd@protonmail.com>
2021-08-23 19:30:37 +0100

    remove unneeded methods and optimize

--- src/networking.cs
+++ src/networking.cs
@@ -5,7 +5,6 @@ using System.Text;
 using System.Diagnostics;
 using System.Collections.Generic;
 using System.Net.NetworkInformation;
-using approachcircle.Debug;
 
 namespace bluebird {
     namespace Networking {
@@ -42,7 +41,7 @@ namespace bluebird {
             }
 
             #nullable enable
-            public dynamic? GetPingMS() {
+            public long? GetPingMS() {
                 Options.DontFragment = true;
 
                 string data = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
@@ -63,13 +62,11 @@ namespace bluebird {
                         return ms;
                     } else {
                         PingSender.Dispose();
-                        //// return "error: ping failed, check your connection to " + address + " and try again";
                         return null;
                     }
 
                 } catch (PingException) {
                     PingSender.Dispose();
-                    //// return "error: the address that the ping was supposed to be sent to has either been changed from its original value of 8.8.8.8 to an unreachable address, or the connection to the destination address has been blocked";
                     return null;
                 }
             }
@@ -77,82 +74,37 @@ namespace bluebird {
         }
 
         public class DLSpeedClass {
+
             protected const string TempFile = "tempfile.tmp";
-            public void InvokeDLSpeedtest() {
-                WebClient WebClientObject = new WebClient();
-                Console.WriteLine("this command can sometimes fail on different networks");
-                Console.WriteLine("testing download speed...");
 
-                Stopwatch sw = Stopwatch.StartNew();
-                try {
-                    WebClientObject.DownloadFile("http://dl.google.com/googletalk/googletalk-setup.exe", TempFile);
-                    sw.Stop();
-                    WebClientObject.Dispose();
-                } catch (WebException) {
-                    WebClientObject.Dispose();
-                    sw.Stop();
-                    Console.ForegroundColor = ConsoleColor.DarkRed;
-                    Console.WriteLine("error: there is a problem with your internet connection. check your connection and try again");
-                    Console.ResetColor();
-                }
-                
-                FileInfo fileInfo = new FileInfo(TempFile);
-
-                try {
-                    long speedBPS = fileInfo.Length / sw.Elapsed.Seconds;
-                    double DoubleSpeedBPS = Convert.ToDouble(speedBPS);
-                    double DoubleSpeedMBPS = DoubleSpeedBPS / 1000000;
-                    Console.WriteLine("download speed is: '{0}' mbps (megabytes per second)", DoubleSpeedMBPS);
-                } catch (DivideByZeroException) {
-                    Console.ForegroundColor = ConsoleColor.DarkRed;
-                    Console.WriteLine("error: an error has occurred, please try this command again later");
-                }
-
-                File.Delete(TempFile);
-            }
-
-            public int GetDLSpeedBPS() {
+            #nullable enable
+            public double? GetDLSpeedMBPS() {
                 WebClient WebClientObject = new WebClient();
                 Stopwatch sw = Stopwatch.StartNew();
                 try {
                     WebClientObject.DownloadFile("http://dl.google.com/googletalk/googletalk-setup.exe", TempFile);
                     sw.Stop();
                     WebClientObject.Dispose();
+
+                    FileInfo fileInfo = new FileInfo(TempFile);
+
+                    try {
+                        long speedBPS = fileInfo.Length / sw.Elapsed.Seconds;
+                        double DoubleSpeedBPS = Convert.ToDouble(speedBPS);
+                        double DoubleSpeedMBPS = DoubleSpeedBPS / 1000000;
+                        File.Delete(TempFile);
+                        //Trace.WriteLine(DoubleSpeedMBPS + " returned from " + this);
+                        return DoubleSpeedMBPS;
+                    } catch (DivideByZeroException) {
+                        File.Delete(TempFile);
+                        return null;
+                    }
                 } catch (WebException) {
                     sw.Stop();
-                    Console.ForegroundColor = ConsoleColor.DarkRed;
-                    Console.WriteLine("error: there is a problem with your internet connection. check your connection and try again");
-                    Console.ResetColor();
+                    return null;
                 }
-
-                System.IO.FileInfo fileInfo = new System.IO.FileInfo(TempFile);
-                long speedBPS2 = fileInfo.Length / sw.Elapsed.Seconds;
-                int IntSpeedBPS2 = Convert.ToInt32(speedBPS2);
-                File.Delete(TempFile);
-                return IntSpeedBPS2;
             }
-
-            public int GetDLSpeedMBPS() {
-                WebClient WebClientObject = new WebClient();
-                Stopwatch sw = Stopwatch.StartNew();
-                try {
-                    WebClientObject.DownloadFile("http://dl.google.com/googletalk/googletalk-setup.exe", TempFile);
-                    sw.Stop();
-                    WebClientObject.Dispose();
-                } catch (WebException) {
-                    sw.Stop();
-                    Console.ForegroundColor = ConsoleColor.DarkRed;
-                    Console.WriteLine("error: there is a problem with your internet connection. check your connection and try again");
-                    Console.ResetColor();
-                }
-
-                System.IO.FileInfo fileInfo = new System.IO.FileInfo(TempFile);
-                long speedBPS3 = fileInfo.Length / sw.Elapsed.Seconds;
-                int IntSpeedBPS3 = Convert.ToInt32(speedBPS3);
-                int IntSpeedMBPS3 = IntSpeedBPS3 / 1000000;
-                File.Delete(TempFile);
-                return IntSpeedMBPS3;
-            }
+            #nullable disable
         }
     }
 }
